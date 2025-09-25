ea023a7f39179906222fcf55ed01fb682c47fedf
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-25 13:23:31 +0530

    conditons

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,6 +4,9 @@ number=$1
 
 if [ $number -lt 10 ]; then
     echo "number $number is lessthan 10"
+elif [ $number -eq 10 ]; then
+    echo "number $number is equal"
+
 else
     echo "number $number is greaterthan or eqaul to 10"
 fi
