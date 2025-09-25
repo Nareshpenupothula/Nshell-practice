eabf910599faa79aa0c52d7e3b2941ee53f074c8
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-25 13:12:45 +0530

    conditons

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,7 +3,7 @@
 number=$1
 
 if [ $number is lt 10 ]; then
-    echo"number $number is lessthen 10"
+    echo "number $number is lessthen 10"
 else
-    echo"number $number is greaterthan or eqaul to 10"
+    echo "number $number is greaterthan or eqaul to 10"
 fi
