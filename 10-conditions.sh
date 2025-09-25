a808f201df035b26bd493c569fe5880bbe46674c
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-25 13:44:01 +0530

    conditons

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,12 +6,12 @@ number2=$2
 
 if [ $number1 -lt $number2 ]; then
     echo "number $number1 is lessthan $number2"
-    echo "sum: ${$number1+$number2}"
+    sum=$(($number1+$number2))
+    echo "sum is : ${sum}"
 elif [ $number -eq 10 ]; then
     echo "numbers is equal"
-
 else
     echo "number $number1 is greaterthan $number2"
-    echo "sub: ${$number2-$number1}"
-
+    sub=$(($number1-$number2))
+    echo "sub is : ${sub}"
 fi
