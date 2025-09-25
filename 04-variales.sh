5813f8d5b99dcf4dfbfad88d5e22dddcff0d10fc
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-25 09:12:16 +0530

    value

---
+++ 04-variales.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you? It’s been a long time since we met." 
+echo "$PERSON2:: Hi $PERSON1 I’m good and recently joined a new job. How about you?"
+echo "$PERSON1 Doing Great." 
+echo "$PERSON2: How often do you visit this restaurant?"
