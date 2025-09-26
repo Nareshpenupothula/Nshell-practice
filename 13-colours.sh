556e1b4c031e073b0f81662dd2cbc909341b05c7
nari.naresh442@gmail.com <nari.naresh442@gmail.com>
2025-09-26 16:27:31 +0530

    functions

--- 13-colours.sh
+++ 13-colours.sh
@@ -2,7 +2,8 @@
 r="\e[31m"
 g="\e[32m"
 y="\e[33m"
+n="\e[0m"
 
-echo -e "$r successfully completd"
-echo -e "$g failures with error"
-echo -e "$y partially correct"
+echo -e "$r successfully completd $n correct"
+echo -e "$g failures with error $n try to corect it"
+echo -e "$y partially correct $n keep it up"
